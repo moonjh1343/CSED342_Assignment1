8ae538e82f8e579933003a5bd836f15b78bae58c
Moonjh <moonjh0627@postech.ac.kr>
2026-09-16 07:02:23 +0900

    Problem 5b implemented

--- submission.py
+++ submission.py
@@ -69,7 +69,7 @@ def manhattan_distance(loc1: Position, loc2: Position) -> float:
     where the locations are pairs of numbers (e.g., (3, 5)).
     """
     # BEGIN_YOUR_CODE
-    raise Exception("Not implemented yet")
+    return abs(loc1[0] - loc2[0]) + abs(loc1[1] - loc2[1])
     # END_YOUR_CODE
 
 
